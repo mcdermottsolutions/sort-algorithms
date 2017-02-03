1bb328f53b9bd90fccc745027589830d767ffb41
Mark McDermott <markmcdermott@Marks-MacBook-Pro.local>
2017-02-03 04:25:48 -0600

    fixed insert sort

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,10 +1,11 @@
 module.exports = (list) => {
   for (let i=1; i<list.length; i++) {
-    for (let j=i; list[j]>list[j-1]; j--) {
-      let temp = list[j-1];
-      list[j-1] = list[j];
-      list[j] = temp;
+    let temp = list[i];
+    let j=i;
+    for (; temp>list[j-1]; j--) {
+      list[j] = list[j-1];
     }
+    list[i] = temp;
   }
   return list;
 }
